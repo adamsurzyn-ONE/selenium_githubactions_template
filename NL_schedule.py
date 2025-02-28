9f162b4cc78cfedff9deff5b73de48a99afee050
Karolik-Aleksandra-one <aleksandra.karolik@one-line.com>
2025-02-28 12:58:18 +0100

    Update NL_schedule.py

--- NL_schedule.py
+++ NL_schedule.py
@@ -14,8 +14,8 @@ def web_driver():
     options.add_argument("--window-size=1920,1200")
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
-    
-    # Ścieżka do ChromeDrivera (dla GitHub Actions)
+    options.binary_location = "/usr/bin/chromium-browser"
+
     driver = webdriver.Chrome(options=options)
     return driver
 
